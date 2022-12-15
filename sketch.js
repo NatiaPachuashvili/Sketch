03483f93aedda21ef6d69c77cfe9ccd2e253ae1d
NatiaPachuashvili <120434204+NatiaPachuashvili@users.noreply.github.com>
2022-12-14 19:34:13 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function () { 
+   createCanvas(400, 400);
+}
+
+function draw() {
+ background(200);
+fill(255,100, 200, 200); 
+  rect(100, 100, 200, 200);
+}
+
+function ()mousePassed {
+ rect(mouseX, mouseY, 200, 200); 
+}
+
+
